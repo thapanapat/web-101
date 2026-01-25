73e3943f2f49ca965dab39569d70acf2d1a33abe
thapanapat <thapanapat.jewyee@gmail.com>
2026-01-25 21:21:01 +0700

    basic JS, define variable name and conditional

---
+++ week3/app.js
@@ -0,0 +1,79 @@
+// data tyoe in js
+//number = int float
+//string = "data"
+//boolean = true false
+
+//boolean and(X). or(+)
+//Treu = 1 , false = 0
+// and = &
+//or = ||
+//not = !
+
+//define value name
+//keyword = var (variable) . let (letter) , const (constant)
+// 1.keyword
+// 2.naeme
+// 3.default value
+
+// var fullname = "Jewyee";
+// let lastname = "Hello";
+// const year = 2016;
+
+// console.log(fullname);
+
+// //var -> ไม่คงทีี่ เป็นglobal use
+// //let -> ใช้ในฟังชั่น
+// //const -> คงที่
+
+// if(true){
+//     var name = "jewyee";
+// }
+// console.log(name);
+
+// if(true){
+//     let secret = 'secret';
+// }
+// console.log(secret);
+
+// const num = 25;
+// num = 27;
+// console.log(num);
+
+/* use for long comment */
+
+//coditional
+
+//if
+//if..else
+//if..else if
+//ir..else if...else
+
+a = 100;
+b = 200;
+c = 300;
+d = 300;
+
+if(a > d){
+    console.log("Hello, true");
+}else{
+    console.log("false");
+}
+
+if(b > a){
+    console.log("True");
+}
+
+if(a>c){
+    console.log("a is greater than c");
+}else if(c<b){
+
+
+    console.log("b is greater than c");
+}else{
+    console.log("C is the greatest");
+}
+let word = 'test';
+console.log("True" , word);
+
+//+ - * / % **
+//> < >= <= == !=, the value is boolean
